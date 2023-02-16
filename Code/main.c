597602d5bb53853686da14fabea92dd1cbfbd35c
Menna Eid <90302774+2mennaeid@users.noreply.github.com>
2023-02-16 16:13:05 +0200

    Add files via upload

---
+++ Code/main.c
@@ -0,0 +1,24 @@
+/*
+ * GccApplication6.c
+ *
+ * Created: 11/10/2022 06:28:22 م
+ * Author : Pc
+ */ 
+
+/*#include "MCAL/DIO/DI0_interface.h"
+#include "MCAL/Timer/timer.h"
+#include "ECUAL/Led/Led_interface.h"
+#include "ECUAL/SevenSegment/Seven_seg_interface.h"*/
+#include "MCAL/DIO/DI0_interface.h"
+#include "MCAL/Timer/timer.h"
+#include "ECUAL/Button/button_interface.h"
+//#include "ECUAL/LCD/LCD_Interface.h"
+#include "Application/assignments.h"
+#include "util/delay.h"
+
+
+int main(void)
+{
+	
+	Control_2Mltiplexed_7Seg_usingTwoPush();
+}	
