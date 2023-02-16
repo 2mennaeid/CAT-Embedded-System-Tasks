648626e051c5a91073c23ac9287683ff08124c9d
Menna Eid <90302774+2mennaeid@users.noreply.github.com>
2023-02-16 16:11:42 +0200

    Add files via upload

---
+++ Code/Deubg/main.d
@@ -0,0 +1,42 @@
+main.d main.o: .././main.c .././MCAL/DIO/DI0_interface.h \
+ .././MCAL/DIO/DIO_priv.h .././MCAL/DIO/errorstate.h \
+ .././MCAL/DIO/Registers1.h .././MCAL/Timer/timer.h \
+ .././MCAL/Timer/../DIO/Registers1.h .././ECUAL/Button/button_interface.h \
+ .././ECUAL/Button/../../MCAL/DIO/DI0_interface.h \
+ .././Application/assignments.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+.././MCAL/DIO/DI0_interface.h:
+
+.././MCAL/DIO/DIO_priv.h:
+
+.././MCAL/DIO/errorstate.h:
+
+.././MCAL/DIO/Registers1.h:
+
+.././MCAL/Timer/timer.h:
+
+.././MCAL/Timer/../DIO/Registers1.h:
+
+.././ECUAL/Button/button_interface.h:
+
+.././ECUAL/Button/../../MCAL/DIO/DI0_interface.h:
+
+.././Application/assignments.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ Code/Deubg/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Application\app.c
+
+Application\assignments.c
+
+ECUAL\Button\button_priv.c
+
+ECUAL\LCD\LCD_Prog.c
+
+ECUAL\Led\Led_priv.c
+
+ECUAL\SevenSegment\Seven_seg_priv.c
+
+main.c
+
+MCAL\DIO\DIO_prog.c
+
+MCAL\Timer\timer.c
+
