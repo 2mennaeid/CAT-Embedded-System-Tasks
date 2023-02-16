ab45c7737025eca5e013121d76d6bc6a2c8f96fd
Menna Eid <90302774+2mennaeid@users.noreply.github.com>
2023-02-16 16:18:06 +0200

    Add files via upload

---
+++ Code/ECUAL/Button/button_interface.h
@@ -0,0 +1,18 @@
+﻿/*
+ * button_interface.h
+ *
+ * Created: 30/01/2023 07:03:41 م
+ *  Author: Pc
+ */ 
+
+
+#ifndef BUTTON_INTERFACE_H_
+#define BUTTON_INTERFACE_H_
+#include "../../MCAL/DIO/DI0_interface.h"
+ES_t Button_eninit(uint8_t portnumber,uint8_t pinnumber,uint8_t type_conecction);
+ES_t Button_Getstate(uint8_t portnumber,uint8_t pinnumber,uint8_t *value);
+
+
+
+
+#endif /* BUTTON_INTERFACE_H_ */
---
+++ Code/ECUAL/Button/button_priv.c
@@ -0,0 +1,38 @@
+﻿/*
+ * button_priv.c
+ *
+ * Created: 30/01/2023 07:04:42 م
+ *  Author: Menna Eid
+ */ 
+
+#include "button_interface.h"
+ES_t Button_eninit(uint8_t portnumber,uint8_t pinnumber,uint8_t type_conecction)
+{
+	ES_t LocalErrorstate;
+	if(portnumber>='A'&& portnumber<='D')
+	{
+		DIO_SetPinDirection(portnumber,pinnumber,IN);
+		DIO_SetPinValue(portnumber,pinnumber,type_conecction);
+		LocalErrorstate=ES_OK;
+	}
+	else
+	{
+		LocalErrorstate=ES_NOK;
+	}
+	return LocalErrorstate;
+	
+}
+ES_t Button_Getstate(uint8_t portnumber,uint8_t pinnumber,uint8_t *value)
+{
+	ES_t LocalErrorstate;
+	if(portnumber>='A'&& portnumber<='D')
+	{
+		DIO_readpinvalue(portnumber,pinnumber,value);
+		LocalErrorstate=ES_OK;
+	}
+	else
+	{
+		LocalErrorstate=ES_NOK;
+	}
+	return LocalErrorstate;
+}
