9f3b0f032898fd7bd632313be483caf634692e86
Menna Eid <90302774+2mennaeid@users.noreply.github.com>
2023-02-16 16:27:34 +0200

    Add files via upload

---
+++ Code/Deubg/ECUAL/Seven Segment/Seven_seg_priv.d
@@ -0,0 +1,23 @@
+ECUAL/SevenSegment/Seven_seg_priv.d ECUAL/SevenSegment/Seven_seg_priv.o: \
+ ../ECUAL/SevenSegment/Seven_seg_priv.c \
+ ../ECUAL/SevenSegment/../../MCAL/DIO/DI0_interface.h \
+ ../ECUAL/SevenSegment/../../MCAL/DIO/DIO_priv.h \
+ ../ECUAL/SevenSegment/../../MCAL/DIO/errorstate.h \
+ ../ECUAL/SevenSegment/../../MCAL/DIO/Registers1.h \
+ ../ECUAL/SevenSegment/Seven_seg_interface.h \
+ ../ECUAL/SevenSegment/Seven_Segment_config.h \
+ ../ECUAL/SevenSegment/Seven_Segment_priv.h
+
+../ECUAL/SevenSegment/../../MCAL/DIO/DI0_interface.h:
+
+../ECUAL/SevenSegment/../../MCAL/DIO/DIO_priv.h:
+
+../ECUAL/SevenSegment/../../MCAL/DIO/errorstate.h:
+
+../ECUAL/SevenSegment/../../MCAL/DIO/Registers1.h:
+
+../ECUAL/SevenSegment/Seven_seg_interface.h:
+
+../ECUAL/SevenSegment/Seven_Segment_config.h:
+
+../ECUAL/SevenSegment/Seven_Segment_priv.h:
