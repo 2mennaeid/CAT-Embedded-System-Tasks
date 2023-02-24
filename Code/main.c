e6f38e111f03509616f26a7c641cfcba3cb6cbda
Menna Eid <90302774+2mennaeid@users.noreply.github.com>
2023-02-24 13:59:08 +0200

    Add files via upload

---
+++ Code/main.c
@@ -0,0 +1,29 @@
+/*
+ * GccApplication6.c
+ *
+ * Created: 11/10/2022 06:28:22 م
+ * Author : Pc
+ */ 
+
+#include "MCAL/DIO/Registers1.h"
+#include "MCAL/DIO/DIO_priv.h"
+#include "MCAL/DIO/errorstate.h"
+#include "MCAL/DIO/DI0_interface.h"
+#include "ECUAL/LCD/LCD_Interface.h"
+#include "ECUAL/KeyPad/KETPAD_INTERFACE.h"
+#include "Application/Calculator.h"
+int main(void)
+{
+
+	LCD_enInit();
+	KeyPad_u8Init();
+	while(1)
+	{
+		
+	     enCalculator();	
+		
+		
+		
+	}
+	
+}	
